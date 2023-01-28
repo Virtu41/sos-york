02a01b6f0234d199d62bc078ad78b6823bce2933
Oscar Lam <oscarl9173665@gmail.com>
2023-01-28 17:32:31 -0500

    Add files via upload

--- CSVReader.py
+++ CSVReader.py
@@ -8,11 +8,7 @@ def csvread(values):
     df = pd.read_csv("SheetHackathon.csv")
     df.fillna("None", inplace=True)
     # print(df.to_string())
-    
-    # adv = df[df['Name'].str.contains(values[3], na=False, case=False)]
-    # Rows that matches the user's input
-    adv = df[df.apply(lambda row: row.astype(str).str.contains(values[3], case=False, na=False).any(), axis=1)]
-    
+    adv = df[df.apply(lambda row: row.astype(str).str.contains(values[0], case=False, na=False).any(), axis=1)]
     # print(adv.to_string())
 
     print(tabulate(adv, showindex=False, headers=adv.columns, tablefmt="rounded_grid", stralign="center"))
--- SOSYork.py
+++ SOSYork.py
@@ -9,49 +9,35 @@ Version: 1.0
 
 import CSVReader  # module
 import PySimpleGUI as sg
-import pandas as pd
-from tabulate import tabulate
-import numpy as np
 
-help(sg.Column)
 text = ""
 WIDTH, HEIGHT = sg.Window.get_screen_size()  # gets screen size
+sg.set_options(font=("Courier New", 10))
 layout = [[sg.Text("Search for service in York")],
-          [sg.Checkbox('Colleges: ', default = False)],
-          [sg.Checkbox('Departments: ', default = False)],
-          [sg.Checkbox('Support: ', default = False)],
           [sg.Input()],  # get input(search bar)
           [sg.Button('Search')],
           [sg.Button('Exit')],  # button to exit
           [sg.Text(text)]
           ]
-window = sg.Window('SOS York', layout, margins=(WIDTH / 4, HEIGHT / 4))  # creates window with title and window size
+window = sg.Window('SOS York', layout, margins=(WIDTH / 5, HEIGHT / 5))  # creates window with title and window size
 while True:
     event, values = window.read()  # event as buttons, values as search bar values
     if (event == 'Exit') or (event == sg.WIN_CLOSED):  # close program
         break
     if (event == 'Search'):
         window.close()
-        print(values)
-        # SOSYorkEvent.py
-        print(values[3])
         text = CSVReader.csvread(values)
         count = CSVReader.numofresult()
-        print(count)
-        column1 = [
-            [sg.Text(f'Scrollable{i}')] for i in range(count)
+        csv = [
+            [sg.Text(text)]
         ]
         layout = [[sg.Text("Search for service in York")],
-                  [sg.Checkbox('Colleges: ', default=False)],
-                  [sg.Checkbox('Departments: ', default=False)],
-                  [sg.Checkbox('Support: ', default=False)],
                   [sg.Input()],  # get input(search bar)
                   [sg.Button('Search')],
                   [sg.Button('Exit')],  # button to exit
-                  [sg.Text(text)],
-                  [sg.Column(column1, scrollable=True,  vertical_scroll_only=True)],
+                  [sg.Text(f"Searches for {values[0]}: {count}")],
+                  [sg.Multiline(text, size=(int(WIDTH/10), int(HEIGHT/30)), no_scrollbar=False, horizontal_scroll=True, visible=True)]  # used to create horizontal and verical scrollbar
                   ]
-        window = sg.Window('SOS York', layout, margins=(WIDTH / 4, HEIGHT / 4))
-
+        window = sg.Window('SOS York', layout)
 
 window.close()
