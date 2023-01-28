a36ae78680e409044ae609ff1bb73e33fbc4e75a
Oscar Lam <oscarl9173665@gmail.com>
2023-01-28 15:10:03 -0500

    Add files via upload

--- CSVReader.py
+++ CSVReader.py
@@ -1,15 +1,21 @@
 import pandas as pd
 from tabulate import tabulate
+
 import numpy as np
+count = 0
+def csvread(values):
+    global count
+    df = pd.read_csv("SheetHackathon.csv")
+    df.fillna("None", inplace=True)
+    # print(df.to_string())
+    adv = df[df['Name'].str.contains(values[3], na=False, case=False)]
+    # print(adv.to_string())
 
-df = pd.read_csv("SheetHackathon.csv")
-df.fillna("None", inplace=True)
-while True:
-    user = input("Search: ").lower()
-    adv = df[df['Name'].str.contains(user, na=False, case=False)]
-
-    # tabulate -> str table
     print(tabulate(adv, showindex=False, headers=adv.columns, tablefmt="rounded_grid", stralign="center"))
+    count = adv.shape[0]
+    return tabulate(adv, showindex=False, headers=adv.columns, tablefmt="rounded_grid", stralign="center")
 
-    # Number of search results
-    print("Count: %d" % adv.shape[0])
+def numofresult():
+    global count
+    return count
+
--- SOSYork.py
+++ SOSYork.py
@@ -1,5 +1,5 @@
 """
-Front-end of sos-york
+Front-end of SOSYork
 
 Date: 20230128
 filename: SOSYork.py
@@ -7,23 +7,51 @@ Author: Oscar, Nina, Sam
 Version: 1.0
 """
 
+import CSVReader  # module
 import PySimpleGUI as sg
+import pandas as pd
+from tabulate import tabulate
+import numpy as np
 
+help(sg.Column)
+text = ""
 WIDTH, HEIGHT = sg.Window.get_screen_size()  # gets screen size
 layout = [[sg.Text("Search for service in York")],
+          [sg.Checkbox('Colleges: ', default = False)],
+          [sg.Checkbox('Departments: ', default = False)],
+          [sg.Checkbox('Support: ', default = False)],
           [sg.Input()],  # get input(search bar)
           [sg.Button('Search')],
-          [sg.Button('Exit')]  # button to exit
+          [sg.Button('Exit')],  # button to exit
+          [sg.Text(text)]
           ]
 window = sg.Window('SOS York', layout, margins=(WIDTH / 4, HEIGHT / 4))  # creates window with title and window size
 while True:
     event, values = window.read()  # event as buttons, values as search bar values
-    print(event, values)
     if (event == 'Exit') or (event == sg.WIN_CLOSED):  # close program
         break
     if (event == 'Search'):
+        window.close()
+        print(values)
         # SOSYorkEvent.py
-        print('ok')
-        searchWindow = sg.Window('SOS York', layout, margins=(WIDTH / 4, HEIGHT / 4))
+        print(values[3])
+        text = CSVReader.csvread(values)
+        count = CSVReader.numofresult()
+        print(count)
+        column1 = [
+            [sg.Text(f'Scrollable{i}')] for i in range(count)
+        ]
+        layout = [[sg.Text("Search for service in York")],
+                  [sg.Checkbox('Colleges: ', default=False)],
+                  [sg.Checkbox('Departments: ', default=False)],
+                  [sg.Checkbox('Support: ', default=False)],
+                  [sg.Input()],  # get input(search bar)
+                  [sg.Button('Search')],
+                  [sg.Button('Exit')],  # button to exit
+                  [sg.Text(text)],
+                  [sg.Column(column1, scrollable=True,  vertical_scroll_only=True)],
+                  ]
+        window = sg.Window('SOS York', layout, margins=(WIDTH / 4, HEIGHT / 4))
+
 
 window.close()
